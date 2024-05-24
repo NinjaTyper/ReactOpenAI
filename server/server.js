ed481e0d523b69a3d30f936914d33ffa65700549
Martin <martin.nguyen.20289@gmail.com>
2024-05-23 22:22:26 -0600

    added Open AI server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 4000; // You can change this to any port you prefer
 
 const configuration = {
-    apiKey: 'sk-proj-JbEXtxOfe4rXBzkIOcCIT3BlbkFJ8RUDpeTd4YqwDQBBOVCD',
+    apiKey: '',
 };
 
 const openAi = new OpenAI(configuration);
